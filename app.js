fb9c3f3d5255e951799c81ef9541d339b946b943
tatiana84 <tantamuntean2018@gmail.com>
2021-04-02 00:30:52 +0300

    Second commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const input = "Don't forget to enjoy every moment, you don't have nine lives as cats have!"; // 1 step
-//const input = 'turpentine and turtles'; // UUEEEEEEEEEEEEEEEEEEIIIIEEEEEEEEEEEEEEEEEEAAAAAAAAUUEEEEEEEEEEEEEEEEEE
-//const input = 'Hi, Human'; // IIIIUUAAAAAAAA
+//const input = 'turpentine and turtles'; // UUEEIEEAUUEE
+//const input = 'Hi, Human'; // IUUA
 const vowels = ['o', 'o', 'e', 'o', 'e', 'o', 'e', 'e', 'o', 'e', 'o', 'u', 'o', 'a', 'e', 'i', 'e', 'i', 'e', 'a', 'a', 'a', 'e']; // 2 step
 
 const resultArray = []; // 3 step
@@ -9,15 +9,15 @@ for (let i=0; i < input.length; i++) { // 4 step
     for (let v = 0; v < vowels.length; v++) {
         if (input[i] === vowels[v]) {
             if(input[i] === 'o') {
-                console.log(resultArray.push('oo'));
+                console.log(resultArray.push('o'));
             } else if (input[i] === 'e') {
                 console.log(resultArray.push('ee'));
             } else if (input[i] === 'u') {
                 console.log(resultArray.push('uu'));
             } else if (input[i] === 'a') {
-                console.log(resultArray.push('aa'));
+                console.log(resultArray.push('a'));
             } else if (input[i] === 'i') {
-                console.log(resultArray.push('ii'));
+                console.log(resultArray.push('i'));
             } else {
                 console.log(resultArray.push(input[i]));
             }
